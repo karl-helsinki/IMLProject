a91ba2729d1b4e30fe00eeda50fe62777c2d3273
荣光 <rongguang@rongguangdeMacBook-Air.local>
2023-11-22 19:58:19 +0200

    initialized->data, EDA script (on-going) and memo

---
+++ team_work_memo.R
@@ -0,0 +1,81 @@
+
+We are team 16, with team members Dominik Kuczkowski, Karl Zhu and Rong Guang.
+
+We held our first team meeting on 19 November, when we discussed and made 
+decisions on the schedule for the final task. 
+
+# Key points
+
+The key points include:
+  
+  - Task stages and expected duration for each stage.
+  
+  - The way we collaborate (who’s in charge of which stage and how others make 
+    contribution)
+  
+  - Tools, platforms, and information sources.
+  
+  - Weekly meeting schedule.
+
+# Details for task stages and duty allocation
+  
+  - We decided to separate the tasks into three parts (stages) and each team 
+    member is responsible for one part : 
+  
+       - Rong Guang in charge of stage 1 wrangling
+
+       - Dominik Kuczkowski in charge of modeling
+
+       - Karl Zhu in charge of reporting
+       
+  - Each team member is responsible for one part, with others contributing to 
+    the process. 
+    
+      -In each stage, the duty for the responsible peroson is 
+       
+         - to initialize the corresponding part of task
+    
+         - to push the progression of the part of task
+    
+         - to inform other team members about the progression of the task
+    
+         - if the task involving coding, make sure the codes are explicitly 
+           commented and timely pushed to repo.  
+    
+         - to timely bring onto table the key decison points he runs into and 
+           decide with the contribution and/or agreement of all members
+           
+         - to respond to the enquiry, contribution and concern other team 
+           members with respective to this specific part of taskCallbackManager
+           
+         - to make adjustment of the code/text, during or after corresponding
+           stage, according to any legitimate need (e.g. needs from the finding 
+           in next stages). 
+           
+         - At the end of each stage, the responsible persons should summarise 
+           their work, either verbally or textually, to the others.
+           
+      -In each stage, the duty for the constributing perosons (other two team 
+       member) is:
+             
+          - Keep tract of the progression (e.g. check Telegram on regular basis)
+           
+          - Join the discussion of key decision points the responsible person 
+            proposes (e.g. make key decision; work out the problems)
+            
+          - Push the progression of the stage whenever feeling necessary.
+           
+# Details for tools
+            
+   - We will use R as major tool, but also open to using Python in necessary 
+     circumstances. 
+     
+   - We will use GitHub Repo for code sharing and collaborating.
+     
+   - We will choose a platform supporting latex and collaboration for drafting
+     final report 
+     
+   - We will use Telegram for real-time communication
+
+
+  
